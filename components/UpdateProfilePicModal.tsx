20966c5b1d1891dc62aea3ebd85473fc37b60db4
matews-sousa <mateussdamiano@gmail.com>
2022-03-13 14:04:24 -0300

    added update profile picture functionality

---
+++ components/UpdateProfilePicModal.tsx
@@ -0,0 +1,139 @@
+import { Dialog, Transition } from "@headlessui/react";
+import React, { Fragment } from "react";
+
+interface Props {
+  isOpen: boolean;
+  setIsOpen: (v: boolean) => void;
+  onChange: (e: any) => void;
+  imagePreview: string | null | undefined;
+  removeImage: () => void;
+  updateImage: () => void;
+  isUpdating: boolean;
+  progress: number;
+}
+
+const UpdateProfilePicModal = ({
+  isOpen,
+  setIsOpen,
+  onChange,
+  imagePreview,
+  removeImage,
+  updateImage,
+  isUpdating,
+  progress,
+}: Props) => {
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        onClose={closeModal}
+      >
+        <div className="min-h-screen px-4 text-center">
+          <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="my-8 inline-block w-full max-w-lg transform overflow-hidden rounded-2xl bg-white pt-4 align-middle shadow-xl transition-all">
+              <Dialog.Title
+                as="h3"
+                className="flex items-center justify-between border-b border-gray-400 px-6 pb-4 text-center text-lg font-medium leading-6 text-gray-900"
+              >
+                <p>Change Profile Photo</p>
+                <button
+                  className={`btn btn-ghost btn-sm text-blue-500 ${
+                    isUpdating && "loading"
+                  }`}
+                  disabled={isUpdating || imagePreview === ""}
+                  onClick={updateImage}
+                >
+                  Save
+                </button>
+              </Dialog.Title>
+              <div className="overflow-y-auto p-2">
+                <div className="relative mb-12 grid h-56 place-items-center">
+                  {imagePreview && (
+                    <div className="h-full w-44">
+                      <img
+                        src={imagePreview}
+                        className="h-44 w-44 rounded-full object-cover"
+                      />
+                      {isUpdating && (
+                        <div>
+                          <h3 className="text-2xl font-bold">Updating...</h3>
+                          <progress
+                            className="progress progress-info w-full"
+                            value={progress}
+                            max="100"
+                          ></progress>
+                          <p className="text-xl font-semibold">{progress}%</p>
+                        </div>
+                      )}
+                    </div>
+                  )}
+
+                  {!isUpdating && (
+                    <>
+                      <h3 className="text-2xl">Drag a photo here</h3>
+                      <input
+                        type="file"
+                        id="profilePic"
+                        className="absolute inset-0 h-full w-full opacity-0"
+                        onChange={onChange}
+                        accept="image/*"
+                      />
+                    </>
+                  )}
+                </div>
+                <div className="mt-4 flex flex-col space-y-2">
+                  {!isUpdating && (
+                    <>
+                      <div className="w-full">
+                        <label htmlFor="profilePic" className="btn btn-info">
+                          Photo from your computer
+                        </label>
+                      </div>
+                      <button
+                        className="btn btn-ghost mx-auto w-1/2 text-red-600"
+                        onClick={removeImage}
+                        disabled={imagePreview === ""}
+                      >
+                        Remove profile photo
+                      </button>
+                      <button
+                        className="btn btn-ghost mx-auto w-1/2"
+                        onClick={closeModal}
+                      >
+                        Cancel
+                      </button>
+                    </>
+                  )}
+                </div>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+
+export default UpdateProfilePicModal;
--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,26 +1,118 @@
 import { updateProfile } from "firebase/auth";
 import { doc, updateDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Avatar from "../components/Avatar";
 import InputField from "../components/InputField";
 import Layout from "../components/Layout";
+import UpdateProfilePicModal from "../components/UpdateProfilePicModal";
 import { useAuth } from "../context/AuthContext";
-import { auth, db } from "../utils/firebase";
+import { auth, db, storage } from "../utils/firebase";
 
 interface FormData {
   name: string;
   username: string;
 }
 
+// ADD VALIDATION TO USERNAME IF IS ALREADY IN USE
+
 const Edit = () => {
-  const [isSubmiting, setIsSubmiting] = useState(false);
   const { currentUser, setCurrentUser } = useAuth();
   const defaultValues = {
     name: currentUser?.displayName,
     username: currentUser?.username,
   };
   const { register, handleSubmit, reset } = useForm({ defaultValues });
+  const [isSubmiting, setIsSubmiting] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [editProfileIsOpen, setEditProfileIsOpen] = useState(false);
+  const [file, setFile] = useState(null);
+  const [imagePreview, setImagePreview] = useState(currentUser?.photoURL);
+
+  const selectImage = async (e: any) => {
+    const f = e.target?.files[0];
+    if (f) {
+      setFile(f);
+      const base64: any = await convertBase64(f);
+      setImagePreview(base64);
+    }
+  };
+
+  const convertBase64 = (file: File) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
+  const updateImage = () => {
+    if (currentUser && file) {
+      const storageRef = ref(storage, `/posts/${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          setIsUpdating(true);
+          const prog = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+          );
+          setProgress(prog);
+        },
+        (err) => console.log(err),
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (url) => {
+            updateProfile(auth.currentUser, {
+              photoURL: url,
+            }).then(async () => {
+              const userRef = doc(db, "users", currentUser.uid);
+              const userDocUpdated = {
+                photoURL: url,
+              };
+              await updateDoc(userRef, userDocUpdated);
+              setCurrentUser({
+                ...currentUser,
+                ...userDocUpdated,
+              });
+              setIsUpdating(false);
+              setProgress(0);
+            });
+          });
+        },
+      );
+    }
+  };
+
+  const removeImage = () => {
+    if (currentUser && imagePreview && !isUpdating) {
+      setIsUpdating(true);
+      setImagePreview("");
+      updateProfile(auth.currentUser, {
+        photoURL: null,
+      }).then(async () => {
+        const userRef = doc(db, "users", currentUser.uid);
+        const userDocUpdated = {
+          photoURL: null,
+        };
+        await updateDoc(userRef, userDocUpdated);
+        setCurrentUser({
+          ...currentUser,
+          ...userDocUpdated,
+        });
+        setIsUpdating(false);
+        setImagePreview("");
+        setFile(null);
+      });
+    }
+  };
 
   const onSubmit = async (data: FormData) => {
     console.log(data);
@@ -62,7 +154,10 @@ const Edit = () => {
           />
           <div>
             <h3 className="text-xl">{currentUser?.username}</h3>
-            <button className="font-semibold text-blue-500">
+            <button
+              className="font-semibold text-blue-500"
+              onClick={() => setEditProfileIsOpen(true)}
+            >
               Change Profile Photo
             </button>
           </div>
@@ -80,6 +175,16 @@ const Edit = () => {
           </button>
         </form>
       </div>
+      <UpdateProfilePicModal
+        isOpen={editProfileIsOpen}
+        setIsOpen={setEditProfileIsOpen}
+        onChange={selectImage}
+        imagePreview={imagePreview}
+        removeImage={removeImage}
+        updateImage={updateImage}
+        isUpdating={isUpdating}
+        progress={progress}
+      />
     </Layout>
   );
 };
